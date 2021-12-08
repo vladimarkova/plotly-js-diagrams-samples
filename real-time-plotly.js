5e449beb817b2bbeed4a0bf9a64d60b04eafa347
Vladi Markova <vladislavam1996@gmail.com>
2021-12-08 14:28:32 +0200

    better visualization, plot xaxis move from 500 up along with the streamed data

--- real-time-plotly.js
+++ real-time-plotly.js
@@ -10,10 +10,22 @@ function plotData() {
         }
     ])
 
+    let cnt = 0;
+
     setInterval(() => {
         // this is the main way to etend a plot with more points
         Plotly.extendTraces('real-time-plot', { y: [[getData()]] }, [0]);
-    }, 200)
+        cnt++; 
+        
+        if (cnt > 500) {
+            Plotly.relayout('real-time-plot', {
+                xaxis: {
+                    range: [cnt-500, cnt]
+                }
+            });
+        }
+
+    }, 15)
 };
 
 plotData();
